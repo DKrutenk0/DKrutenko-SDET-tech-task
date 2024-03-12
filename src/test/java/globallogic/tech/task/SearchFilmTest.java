d5a47b19477060ab2a8c7c4f9735784c3f8d67d8
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-03-12 16:10:19 +0200

    Create verifyMovieSearch test

---
+++ src/test/java/globallogic/tech/task/SearchFilmTest.java
@@ -0,0 +1,34 @@
+package globallogic.tech.task;
+
+import globallogic.tech.task.pageobjects.HomePage;
+import globallogic.tech.task.pageobjects.MoviePage;
+import org.testng.annotations.Test;
+import globallogic.tech.task.runners.TestRunner;
+
+import static org.testng.AssertJUnit.*;
+
+public class SearchFilmTest extends TestRunner {
+    @Test
+    public void verifyMovieSearch() {
+        HomePage homePage = new HomePage();
+        MoviePage moviePage = new MoviePage();
+        var expectedMovieTitle = "Назад у майбутнє";
+        var expectedMovieYear = "1985";
+        var expectedMovieDescription = "Marty McFly, a 17-year-old high school student, is accidentally sent 30" +
+                " years into the past in a time-traveling DeLorean invented by his close friend," +
+                " the maverick scientist Doc Brown.";
+
+        homePage.fillSearchField(expectedMovieTitle)
+                .clickSearchButton()
+                .clickExactMatchesButton()
+                .clickOnMovieByCriteria(expectedMovieTitle, expectedMovieYear);
+
+        var actualMovieTitle = moviePage.retrieveMovieTitle();
+        var movieData = moviePage.retrieveMovieDataByYear(expectedMovieYear);
+        var actualMovieDescription = moviePage.retrieveMovieDescription();
+
+        assertEquals("Verify movie title", expectedMovieTitle, actualMovieTitle);
+        assertTrue("Verify movie year", movieData.contains(expectedMovieYear));
+        assertEquals("Verify movie description", expectedMovieDescription, actualMovieDescription);
+    }
+}
